517de40dbf553cfd326c8a3238e26c8a937b1590
Sean Smith <ssmith@missiondata.com>
2021-01-26 12:45:37 -0500

    Added freetds@1.2.3

---
+++ Formula/freetds@1.2.3.rb
@@ -0,0 +1,55 @@
+class FreetdsAT123 < Formula
+  desc "Libraries to talk to Microsoft SQL Server and Sybase databases"
+  homepage "https://www.freetds.org/"
+  url "https://www.freetds.org/files/stable/freetds-1.2.3.tar.gz"
+  sha256 "50ca5f54a76088fcf54873d76806996c6f43a7b6defafafa2b11060caf2e05b8"
+
+  bottle do
+    root_url "http://devops.missiondata.net/Homebrew/Bottles"
+    sha256 "03b917eea03d12ae4e99bc5567de6edeb15a42caa1d4332a8e21dcdc488c1e8a" => :big_sur
+  end
+
+  head do
+    url "https://github.com/FreeTDS/freetds.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl@1.1"
+  depends_on "unixodbc"
+
+  on_linux do
+    depends_on "readline"
+  end
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --with-tdsver=7.3
+      --mandir=#{man}
+      --sysconfdir=#{etc}
+      --with-unixodbc=#{Formula["unixodbc"].opt_prefix}
+      --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
+      --enable-sybase-compat
+      --enable-krb5
+      --enable-odbc-wide
+    ]
+
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+    system "make"
+    ENV.deparallelize # Or fails to install on multi-core machines
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/tsql", "-C"
+  end
+end
